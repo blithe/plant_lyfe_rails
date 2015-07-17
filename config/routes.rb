0462767d091339e87a30c6f5f32a8366bd2e72b3
Blithe Rocher <blitherocher@gmail.com>
2015-07-17 11:07:08 -0700

    Make URL use singular leaf instead of leaves

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :dicots, :defaults => { :format => 'json' } do
-        resources :leafs, :defaults => { :format => 'json' }
+        resource :leaf
     end
 
 end
